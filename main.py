44b486f680973179ba58afdbc1b12f5b7ac07b0c
cbdewolf <cbdewolf@gmail.com>
2025-03-10 20:08:05 -0500

    implemented some backend methods, need to do frontend

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from payment_routes import payment_router
 
 app = FastAPI(title="pmp-app")
-
 app.include_router(payment_router, tags=["Payments"], prefix="/payments")
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"])
 
 
 @app.get("/")
